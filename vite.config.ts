5fb04763020375a537a0a9377eca515bfdc3dfd8
Prakhar Gupta <Prakhar.gupta@teammas.co.in>
2026-09-14 16:04:41 +0530

    server config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
